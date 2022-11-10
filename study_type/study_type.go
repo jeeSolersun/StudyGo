b7c49d03f0047b6eca6c1c07597f4ab8771f0d5c
Solersun <solersun@163.com>
2022-11-11 01:21:39 +0800

    Revert "!1 测试一个错误的merge"
    This reverts commit 9441456f4d6c6cbb7a036ade5228a2378304463c, reversing
    changes made to 70ef9b99e57c9eeadd9da4d6e163a8aeb497a031.

--- study_type/study_type.go
+++ study_type/study_type.go
@@ -8,11 +8,3 @@ func Study() {
 	var num2 int64 = int64(num) // 使用type()函数进行转换
 	fmt.Printf("num2: %v\n", num2)
 }
-
-func Study02() {
-	fmt.Printf("\"modify 01 on dev\": %v\n", "modify 01 on dev")
-
-	fmt.Printf("\"modify 02 on dev\": %v\n", "modify 02 on dev")
-
-	fmt.Printf("\"modify 03 on dev\": %v\n", "modify 03 on dev")
-}
